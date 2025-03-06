07ee40274ecb597e095953d957f91dd8ee9f7570
Amethyste34 <laurence.amethyste@gmail.com>
2025-03-06 18:32:33 +0100

    Ajout de pages

---
+++ src/app/(shop)/_layout.tsx
@@ -0,0 +1,41 @@
+import { Tabs } from "expo-router"
+import { SafeAreaView } from "react-native-safe-area-context";
+import { StyleSheet } from "react-native";
+
+const TabsLayout = () => {
+    return (
+        <SafeAreaView edges={['top']} style={styles.safeArea}>
+            <Tabs
+                screenOptions={{
+                    tabBarActiveTintColor: "#1BC464",
+                    tabBarInactiveTintColor: 'gray',
+                    tabBarLabelStyle: { fontSize: 16 },
+                    tabBarStyle: {
+                        height: 70,
+                        borderTopLeftRadius: 20,
+                        borderTopRightRadius: 20,
+                        paddingTop: 5,
+                    },
+                    headerShown: false,
+                }}
+            >
+                <Tabs.Screen
+                    name="index"
+                    options={{
+                        headerShown: false,
+                        
+                    }}
+                />
+                <Tabs.Screen name="orders" options={{}} />
+            </Tabs>
+        </SafeAreaView>
+    );
+};
+
+export default TabsLayout;
+
+const styles = StyleSheet.create({
+    safeArea: {
+        flex: 1,
+    }
+});
---
+++ src/app/categories/[slug].tsx
@@ -0,0 +1,14 @@
+import { View, Text, StyleSheet } from 'react-native'
+import React from 'react'
+
+const Category = () => {
+  return (
+    <View>
+      <Text>Category</Text>
+    </View>
+  )
+}
+
+export default Category;
+
+const styles = StyleSheet.create({});
