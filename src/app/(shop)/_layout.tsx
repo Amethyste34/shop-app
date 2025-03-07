5de4a789064d5b9856e2bcdecb34520d9fcfa2ea
Amethyste34 <laurence.amethyste@gmail.com>
2025-03-07 13:40:40 +0100

    Ajout icônes pour la navBar

--- src/app/(shop)/_layout.tsx
+++ src/app/(shop)/_layout.tsx
@@ -1,6 +1,14 @@
 import { Tabs } from "expo-router"
 import { SafeAreaView } from "react-native-safe-area-context";
 import { StyleSheet } from "react-native";
+import { FontAwesome } from "@expo/vector-icons";
+
+function TabBarIcon(props: {
+    name: React.ComponentProps<typeof FontAwesome>['name'];
+    color: string;
+}) {
+    return <FontAwesome size={24} {...props} style={{ color: "#1BC464"}} />;
+}
 
 const TabsLayout = () => {
     return (
@@ -11,7 +19,7 @@ const TabsLayout = () => {
                     tabBarInactiveTintColor: 'gray',
                     tabBarLabelStyle: { fontSize: 16 },
                     tabBarStyle: {
-                        height: 70,
+                        height: 80,
                         borderTopLeftRadius: 20,
                         borderTopRightRadius: 20,
                         paddingTop: 5,
@@ -22,11 +30,20 @@ const TabsLayout = () => {
                 <Tabs.Screen
                     name="index"
                     options={{
-                        headerShown: false,
-                        
+                        title: "shop",
+                        tabBarIcon(props) {
+                            return <TabBarIcon {...props} name="shopping-cart" />;
+                        },                        
                     }}
                 />
-                <Tabs.Screen name="orders" options={{}} />
+                <Tabs.Screen
+                    name="orders"
+                    options={{
+                        title: "Orders",
+                        tabBarIcon(props) {
+                            return <TabBarIcon {...props} name="book" />;
+                        },
+                    }} />
             </Tabs>
         </SafeAreaView>
     );
